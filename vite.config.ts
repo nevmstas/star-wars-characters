63db816511720c1cfc650ac48fddb0d2e1140b84
nevmstas <nevmyvakastas@gmail.com>
2024-07-10 20:38:46 +0600

    modify config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vitest/config';
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  plugins: [react()],
-  base: '/',
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './setupTests.ts',
-  },
-})
+export default defineConfig(({ mode }) => {
+  const base = mode === "development" ? undefined : "/star-wars-characters";
+  return {
+    plugins: [react()],
+    base,
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./setupTests.ts",
+    },
+  };
+});
